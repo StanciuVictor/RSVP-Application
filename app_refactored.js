d668b6591ba72b32a1eb20a8a42e23ac1979a406
StanciuVictor <victor.stanciu@yahoo.com>
2021-10-05 16:10:48 +0300

    Update app_refactored.js
    Comments

--- app_refactored.js
+++ app_refactored.js
@@ -14,25 +14,25 @@ document.addEventListener("DOMContentLoaded", () => {
   div.appendChild(filterLabel);
   div.appendChild(filterCheckbox);
   mainDiv.insertBefore(div, ul);
-  
-  //  Hide those who haven't responded
+
+  // Hide those who haven't responded
   filterCheckbox.addEventListener("change", (e) => {
-    const isChecked = e.target.checked; //  TRUE if checked, FALSE if not
-    const lis = ul.children;            //  Provides a reference to a collection of elements children
+    const isChecked = e.target.checked;               // TRUE if checked, FALSE if not
+    const lis = ul.children;                          // Provides a reference to a collection of elements children
     if (isChecked) {
-      //  If checked
+      // If checked
       for (let i = 0; i < lis.length; i++) {
-        //  Go through invitees list
+        // Go through invitees list
         let li = lis[i];
         if (li.className === "responded") {
-          //  If invitee is confirmed
-          li.style.display = ""; //  Do nothing
+          // If invitee is confirmed
+          li.style.display = "";                      // Do nothing
         } else {
-          li.style.display = "none"; //  Hide invitee
+          li.style.display = "none";                  // Hide invitee
         }
       }
     } else {
-      //  If not checked, display all invitees
+      // If not checked, display all invitees
       for (let i = 0; i < lis.length; i++) {
         let li = lis[i];
         li.style.display = "";
@@ -40,15 +40,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  //  Creates <li> elements with names, checkbox, edit button and remove button
+  // Creates <li> elements with names, checkbox, edit button and remove button
   function createLI(text) {
-    //  Creates specified element with specified property and value
+    // Creates specified element with specified property and value
     function createElement(elementName, property, value) {
       const element = document.createElement(elementName);
       element[property] = value;
       return element;
     }
-    //  Creates element and appends it to the <li>
+    // Creates element and appends it to the <li>
     function appendToLI(elementName, property, value) {
       const element = createElement(elementName, property, value);
       li.appendChild(element);
@@ -57,14 +57,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const li = document.createElement("li");
 
-    //  To be able to modify the invitee name (text node) easily, insert it into
+    // To be able to modify the invitee name (text node) easily, insert it into
     //    a <span> tag.
     appendToLI("span", "textContent", text);
     /*
-        const label = appendToLI("label", 'textContent', 'Confirmed');
-        const checkbox = createElement("input", 'type', 'checkbox');
-        label.appendChild(checkbox);
-        ******REFACTORED BELOW******
+    const label = appendToLI("label", 'textContent', 'Confirmed');
+    const checkbox = createElement("input", 'type', 'checkbox');
+    label.appendChild(checkbox);
+    ******REFACTORED BELOW******
     */
     appendToLI("label", 'textContent', 'Confirm')
       .appendChild(createElement("input", 'type', 'checkbox'));
@@ -75,7 +75,7 @@ document.addEventListener("DOMContentLoaded", () => {
     return li;
   }
 
-  //  Registering Names
+  // Registering Names
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const newInvitee = input.value;
@@ -100,24 +100,24 @@ document.addEventListener("DOMContentLoaded", () => {
     ul.appendChild(li);
   });
 
-  //  Checkbox
+  // Checkbox
   ul.addEventListener("change", (e) => {
     const checkbox = e.target;
     const checked = checkbox.checked;                 // TRUE if checked, FALSE if not
     const listItem = checkbox.parentNode.parentNode;
     const label = checkbox.parentNode;
-    let textNode = label.childNodes[0];               //  Select the text node containing
+    let textNode = label.childNodes[0];               // Select the text node containing
 
     if (checked) {
       listItem.className = "responded";
-      textNode.data = 'Confirmed';                    //  Change text
+      textNode.data = 'Confirmed';                    // Change text
     } else {
       listItem.className = "";
-      textNode.data = 'Confirm';                      //  Change text
+      textNode.data = 'Confirm';                      // Change text
     }
   });
 
-  //  Removing, Editing or Saving Names
+  // Removing, Editing or Saving Names
   ul.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
       const button = e.target;
@@ -148,15 +148,14 @@ document.addEventListener("DOMContentLoaded", () => {
       const action = button.textContent;  // This can be moved up above nameActions object, with the other declarations
       nameActions[action]();              // This replaces all IF ELSE
       /*           ^^^^
-        action will only be one of this 3 posible strings: remove, edit, save.
-        In the IF, action is compared to each possible string, so when a match is
-        found, that same word will be used to run the branch.
-        In the line above, the string in action will be used to acces the function
-        directly from the object. Then the function will be executed.
+      action will only be one of this 3 posible strings: remove, edit, save.
+      In the IF, action is compared to each possible string, so when a match is
+      found, that same word will be used to run the branch.
+      In the line above, the string in action will be used to acces the function
+      directly from the object. Then the function will be executed.
 
-        This works and it's a better aproach because the name of the buttons
-        match the name of the nameActions properties.
-
+      This works and it's a better aproach because the name of the buttons
+      match the name of the nameActions properties.
       */
 
       // if (action === "Remove") {
